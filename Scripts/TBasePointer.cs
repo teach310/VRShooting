4a8e7e0de185842a5aa703af78c59e478de2988e
Taichi Sato <okms310gmail.com>
2017-09-18 00:42:47 +0900

    Reticle作成

---
+++ Scripts/TBasePointer.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public abstract class TBasePointer : MonoBehaviour {
+
+	/// Returns the transform that represents this pointer.
+	/// It is used by GvrBasePointerRaycaster as the origin of the ray.
+	public virtual Transform PointerTransform {
+		get {
+			return transform;
+		}
+	}
+
+	/// Returns the max distance from the pointer that raycast hits will be detected.
+	public abstract float MaxPointerDistance { get; }
+
+	/// Called when the pointer is facing a valid GameObject. This can be a 3D
+	/// or UI element.
+	///
+	/// **raycastResult** is the hit detection result for the object being pointed at.
+	/// **isInteractive** is true if the object being pointed at is interactive.
+	public abstract void OnPointerEnter(RaycastResult raycastResult, bool isInteractive);
+
+	/// Called every frame the user is still pointing at a valid GameObject. This
+	/// can be a 3D or UI element.
+	///
+	/// **raycastResult** is the hit detection result for the object being pointed at.
+	/// **isInteractive** is true if the object being pointed at is interactive.
+	public abstract void OnPointerHover(RaycastResult raycastResultResult, bool isInteractive);
+
+	/// Called when the pointer no longer faces an object previously
+	/// intersected with a ray projected from the camera.
+	/// This is also called just before **OnInputModuleDisabled**
+	/// previousObject will be null in this case.
+	///
+	/// **previousObject** is the object that was being pointed at the previous frame.
+	public abstract void OnPointerExit(GameObject previousObject);
+
+	/// Called when a click is initiated.
+	public abstract void OnPointerClickDown();
+
+	/// Called when click is finished.
+	public abstract void OnPointerClickUp();
+
+	/// Return the radius of the pointer. It is used by GvrPointerPhysicsRaycaster when
+	/// searching for valid pointer targets. If a radius is 0, then a ray is used to find
+	/// a valid pointer target. Otherwise it will use a SphereCast.
+	/// The *enterRadius* is used for finding new targets while the *exitRadius*
+	/// is used to see if you are still nearby the object currently pointed at
+	/// to avoid a flickering effect when just at the border of the intersection.
+	///
+	/// NOTE: This is only works with GvrPointerPhysicsRaycaster. To use it with uGUI,
+	/// add 3D colliders to your canvas elements.
+	public abstract void GetPointerRadius(out float enterRadius, out float exitRadius);
+
+	protected virtual void Start(){
+	}
+}
---
+++ Scripts/TPhysicsRaycaster.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UniRx;
+using UniRx.Triggers;
+
+//public class TRay{
+//	Ray ray;
+//
+//	public TRay(Vector3 origin, Vector3 direction){
+//		ray = new Ray (origin, direction);
+//	}
+//
+//
+//	class CastResult{
+//		public RaycastHit Hit{ get;}
+//		public bool IsHit{ get;}
+//		public CastResult(RaycastHit hit, bool isHit){
+//			Hit = hit;
+//			IsHit = isHit;
+//		}
+//	}
+//
+//	public IObservable<CastResult> Cast(float maxDistance = Mathf.Infinity){
+//		RaycastHit hit;
+//		Observable.Return<CastResult> (
+//			if(Physics.Raycast(ray, maxDistance){
+//			}else{
+//				
+//			}
+//		);
+//	}
+//}
+
+public class TPhysicsRaycaster : MonoBehaviour {
+
+	#if UNITY_EDITOR
+	public bool drawDebugRays = false;
+	#endif
+
+	[SerializeField] TBasePointer pointer;
+	[SerializeField] float distance;
+	[SerializeField] Ray lastRay;
+	[SerializeField] RaycastHit lastRaycastHit;
+
+	BoolReactiveProperty hitRp = new BoolReactiveProperty(false);
+
+	void Start(){
+		SetListener ();
+	}
+
+	void SetListener(){
+		hitRp
+			.Where(x=>x)
+			.Subscribe (_ => {
+				if(pointer != null)
+					pointer.OnPointerEnter(GetResult(), true);
+			});
+		hitRp
+			.Where (x => !x)
+			.Skip(1) // 初期値はスキップ
+			.Subscribe (_ => {
+				if(pointer != null)
+					pointer.OnPointerExit(lastRaycastHit.collider.gameObject);
+		});
+
+		this.UpdateAsObservable()
+			.Where(_=>hitRp.Value)
+			.Subscribe(_=>{
+				if(pointer != null)
+					pointer.OnPointerHover(GetResult(), true);
+			});
+
+		this.UpdateAsObservable ()
+			.Subscribe (_ => {
+				Cast();
+				MaybeDrawDebugRaysForEditor(Color.red);
+		});
+	}
+
+	RaycastResult GetResult(){
+		return new RaycastResult () {
+			gameObject = lastRaycastHit.collider.gameObject,
+			module = null,
+			worldPosition = lastRaycastHit.point,
+			worldNormal = lastRaycastHit.normal
+		};
+	}
+
+	void Cast(){
+		lastRay = new Ray (this.transform.position, this.transform.forward);
+		RaycastHit hit;
+		bool isHit = Physics.Raycast (lastRay, out hit, distance);
+		if (isHit) {
+			lastRaycastHit = hit;
+		}
+		hitRp.Value = isHit;
+	}
+
+	private void MaybeDrawDebugRaysForEditor(Color color) {
+		
+		#if UNITY_EDITOR
+		if (drawDebugRays) {
+			Debug.DrawRay(lastRay.origin, lastRay.direction * distance, color);
+		}
+		#endif  // UNITY_EDITOR
+	}
+}
